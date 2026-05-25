8b8d4e277d97ef7615aa3e60ceab543dd0918abc
Sherab nima Rigzin <02240358.cst@rub.gov.bt>
2026-05-25 20:29:41 +0530

    her

---
+++ francine-website/public/photos/README.txt
@@ -0,0 +1,18 @@
+📁 PUT FRANCINE'S PHOTOS IN THIS FOLDER
+
+Name them exactly:
+  photo1.jpg
+  photo2.jpg
+  photo3.jpg
+  photo4.jpg
+  photo5.jpg
+  photo6.jpg
+  photo7.jpg
+  photo8.jpg
+  photo9.jpg
+  photo10.jpg
+
+They can be .jpg, .jpeg, .png or .webp
+Just make sure the filename matches what's in gallery.html
+
+💖 That's it! Refresh the browser and her pictures will appear in all 10 frames.
---
+++ francine-website/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the "public" folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Main route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`\n💖 Francine's website is running!`);
+  console.log(`   Open http://localhost:${PORT} in your browser\n`);
+});
